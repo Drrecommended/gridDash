eb4fce73a3e8eaf8dbddcc15991f33044f10b010
Tyler French <tylerfrench89@gmail.com>
2022-09-07 21:27:48 -0700

    testing

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const user1 = document.querySelectorAll('.user1')
+const userOneProfile = document.querySelectorAll('.userOne')
+const userProfiles = document.querySelectorAll('.user')
+const user1 = document.querySelectorAll('user1')
 
-const userProfiles = []
+const userData = []
 
 const getUsers = () => {
   fetch('https://randomuser.me/api/?inc=name,picture&results=5')
@@ -15,21 +17,39 @@ const getUsers = () => {
           lastName: user.name.last,
           picture: user.picture.thumbnail,
         }
-        userProfiles.push(newUser)
+        userData.push(newUser)
       })
-      displayUsers(userProfiles)
+      displayUsers()
     })
 }
 
-const displayUsers = (users) => {
-  for (let i = 0; i < user1.length; i++) {
-    if (user1[i].classList.contains('name')) {
-      user1[i].innerText = `${users[0].firstName} ${users[0].lastName}`
-    } else {
-      user1[i].style.backgroundImage = `url('${users[0].picture}')`
-    }
-  }
+const displayUsers = () => {
+
   
+  //   userProfile.forEach((profile) => {
+  //     userProfiles.forEach((user) => {
+  //       if (
+  //         profile.classList.contains('user1') &&
+  //         profile.classList.contains('circle')
+  //       ) {
+  //         console.log(user)
+  //         profile.style.backgroundImage = `url('${user.picture}')`
+  //       }
+  //     })
+  //   })
+
+  //   for (let i = 0; i < user1.length; i++) {
+  //     if (user1[i].classList.contains('name')) {
+  //       user1[i].innerText = `${users[0].firstName} ${users[0].lastName}`
+  //     } else {
+  //       user1[i].style.backgroundImage = `url('${users[0].picture}')`
+  //     }
+  //   }
+  //   for (let i = 0; i < trendingUsers.length; i++) {
+  //     for(let k = 1; k < users.length; k++) {
+  //         console.log(trendingUsers[k])
+  //     }
+  //   }
 }
 
 getUsers()
